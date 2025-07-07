ad0c54c71d2f717e82af79ec8dc18d28973bb89a
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-07-08 01:12:16 +0200

    name some stuff, delink gp.c

--- libs/c/include/stdio.h
+++ libs/c/include/stdio.h
@@ -1,6 +1,19 @@
 #ifndef _C_STDIO_H
 #define _C_STDIO_H
 
+#define FILENAME_MAX 256
+
+typedef struct FILE {
+    // TODO
+} FILE;
+
+int printf(const char* format, ...);
+int fprintf(FILE* stream, const char* format, ...);
 int sprintf(char *__restrict s, const char *__restrict format, ...);
+int snprintf(char* s, size_t n, const char* format, ...);
+
+int scanf(const char* format, ...);
+int fscanf(FILE* stream, const char* format, ...);
+int sscanf(const char* buffer, const char* format, ...);
 
 #endif
